3afec1d8608ecf744dc57a8e062d6d104e63041f
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-08-21 02:53:44 -0400

    edit socials to include newsletter & more dynamic

--- src/components/SocialCard.js
+++ src/components/SocialCard.js
@@ -2,30 +2,44 @@ import { IoMailOpen } from "react-icons/io5";
 import { IoLogoInstagram } from "react-icons/io5";
 import { IoLogoTwitter } from "react-icons/io5";
 import { FaFacebookF } from "react-icons/fa";
+import { FaNewspaper } from "react-icons/fa";
 
 function getLogoColors() {
     return {
         Facebook: '#3B5998',
         Instagram: '#E1306C',
         Twitter: '#1DA1F2',
-        Email: '#D8BFD8',
+        Email: 'purple',
+        Newsletter: '#FF69B4'
     }
 }
 
-function SocialCard({ src, alt, text, title }) {
+function getLinks() {
+    return {
+        Facebook: 'https://www.facebook.com/GirlUpBoston/',
+        Instagram: 'https://www.instagram.com/girlupnewengland/',
+        Twitter: 'https://x.com/guboscoalition',
+        Email: 'mailto:girlupboston@gmail.com',
+        Newsletter: 'https://actionnetwork.org/forms/join-our-email-list-75'
+    }
+}
+
+function SocialCard({ size, iconSize, alt, text, title }) {
     const color = getLogoColors()[alt];
+    const link = getLinks()[alt];
     
     return (
-        <div>
-        <div className={`rounded-full h-16 w-16 flex items-center justify-center mx-auto`} style={{ backgroundColor: color }}>
-            {alt === 'Facebook' && <FaFacebookF size={40} color="white"/>}
-            {alt === 'Instagram' && <IoLogoInstagram size={40} color="white"/>}
-            {alt === 'Email' && <IoMailOpen size={40} color="white"/>}
-            {alt === 'Twitter' && <IoLogoTwitter size={40} color="white"/>}
+        <a href={link}>
+        <div className='rounded-full h-16 w-16 flex items-center justify-center mx-auto hover:opacity-50' style={{ backgroundColor: color, maxHeight: size, maxWidth: size }}>
+            {alt === 'Facebook' && <FaFacebookF size={iconSize} color="white"/>}
+            {alt === 'Instagram' && <IoLogoInstagram size={iconSize} color="white"/>}
+            {alt === 'Email' && <IoMailOpen size={iconSize} color="white"/>}
+            {alt === 'Twitter' && <IoLogoTwitter size={iconSize} color="white"/>}
+            {alt === 'Newsletter' && <FaNewspaper size={iconSize} color="white"/>}
         </div>
         <div className="text-center font-bold">{title}</div>
         <p className="text-center">{text}</p>
-        </div>
+        </a>
     );
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,11 @@
 import SocialCard from "../components/SocialCard";
 import lobbyday1 from "../images/lobbyday-1.jpg";
 import { Helmet } from "react-helmet";
+import { useState } from "react";
 
 function Contact() {
+   const [message, setMessage] = useState('');
+   
     return (
       <div>
         <Helmet>
@@ -11,33 +14,41 @@ function Contact() {
         <div className="bg-[#ffd1dc] w-full flex items-center justify-center px-4 text-black overflow-hidden pb-20">
           <div>
           <h1 className="text-center py-20">Contact</h1>
-          <div className="mx-auto flex flex-wrap gap-4 justify-center px-4">
-            <SocialCard alt='Facebook' title="Facebook" text="Girl Up Boston Coalition"/>
-            <SocialCard alt='Instagram' title="Instagram" text="@girlupnewengland"/>
-            <SocialCard alt='Email' title="Email Us" text="girlupboston@gmail.com"/>
-            <SocialCard alt='Twitter' title="Twitter" text="@GUBosCoalition"/>
+          <div className="mx-auto flex flex-wrap gap-8 justify-center px-4">
+            <SocialCard alt='Facebook' title="Facebook" text="Girl Up Boston Coalition" size="" iconSize="40"/>
+            <SocialCard alt='Instagram' title="Instagram" text="@girlupnewengland" size="" iconSize="40"/>
+            <SocialCard alt='Email' title="Email Us" text="girlupboston@gmail.com" size="" iconSize="40"/>
+            <SocialCard alt='Twitter' title="Twitter" text="@GUBosCoalition" size="" iconSize="40"/>
+            <SocialCard alt='Newsletter' title="Newsletter" text="Join our email list!" size="" iconSize="40"/>
           </div>
           </div>
         </div>
         <div className="bg-gradient-to-b from-[#FFF5EE] via bg-[#ffd1dc] to bg-[#ffd1dc]">
           <div className = "w-fit mx-auto py-20 grid">
-            <div className="grid grid-cols-1 xl:grid-cols-2 bg-pink-100 shadow-lg">
+            <div className="grid grid-cols-1 xl:grid-cols-2 bg-pink-100 shadow-lg items-center">
               <img src={lobbyday1} alt="capital" height="auto" width="auto" className="max-w-96 m-8" />
               <div className="prose text-center xl:text-left">
-                <h2>Join Us!</h2>
+                <h3>Leave A Comment</h3>
                 <div>
-                <label class="custom-field two"> Name:  
+                <label class="custom-field two">  
                  <input type="text" placeholder="&nbsp;"/>
-                 <span class="placeholder">Your name</span>
+                 <span class="placeholder">Name</span>
                 </label>
                 </div>
                 <div>
-                <label class="custom-field two">Email:  
+                <label class="custom-field two">  
                  <input type="text" placeholder="&nbsp;"/>
-                 <span class="placeholder">Enter email</span>
+                 <span class="placeholder">Email</span>
                 </label>
                 </div>
-                <button class="p-2 bg-pink-400 rounded-lg hover:opacity-50 text-sm mt-5 mb-20">Subscribe to our email list!</button>
+                <div>
+                <div class="custom-field two">  
+                 <textarea rows="5" cols="50" placeholder="&nbsp;"/>
+                 <span class="placeholder">Message</span>
+                </div>
+                </div>
+                <button class="p-2 bg-pink-300 rounded-sm hover:opacity-50 text-sm mt-2 mb-16 w-[350px]" onClick = {() => setMessage('Message sent!')}>Post Comment</button>
+                <div>{message}</div>
               </div>
             </div>
         </div>
